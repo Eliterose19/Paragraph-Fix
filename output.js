58f5f792356ca2006daf9ba63021af27f0cd80ec
Eliterose <eliterose19@gmail.com>
2025-07-21 21:40:26 -0700

    Delete output.js

--- output.js
+++
@@ -1,9 +0,0 @@
-// Your "Output" tab should look like this
-const modifier = (text) => {
-  // Your other output modifier scripts go here (preferred)
-  text = ParagraphFix("output", text);
-  text = AutoCards("output", text);
-  // Your other output modifier scripts go here (alternative)
-  return {text};
-};
-modifier(text);
