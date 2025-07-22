13addf629e4b2941d92961cbb59a53031768d639
Eliterose <eliterose19@gmail.com>
2025-07-21 21:37:56 -0700

    Add files via upload

---
+++ Auto-Cards Merge/output.js
@@ -0,0 +1,9 @@
+// Your "Output" tab should look like this
+const modifier = (text) => {
+  // Your other output modifier scripts go here (preferred)
+  text = ParagraphFix("output", text);
+  text = AutoCards("output", text);
+  // Your other output modifier scripts go here (alternative)
+  return {text};
+};
+modifier(text);
